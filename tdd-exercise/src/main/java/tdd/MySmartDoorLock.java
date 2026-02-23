d8dad81d6aac8f25c77313299b79f55880c5b3fc
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2026-02-23 13:52:15 +0100

    feat: add few test for MySmartDoorLock

---
+++ tdd-exercise/src/main/java/tdd/MySmartDoorLock.java
@@ -0,0 +1,69 @@
+package tdd;
+
+public class MySmartDoorLock implements SmartDoorLock{
+
+    public static final int MAX_ATTEMPTS_FOR_UNLOCK = 3;
+    private Integer pin;
+    private boolean locked;
+    private boolean blocked;
+    private int failedAttempts;
+
+    public MySmartDoorLock() {
+        this.pin = null;
+        this.locked = false;
+        this.blocked = false;
+        this.failedAttempts = 0;
+    }
+
+    @Override
+    public void setPin(int pin) {
+        if (!isLocked() && !isBlocked()) {
+            this.pin = pin;
+        }
+    }
+
+    @Override
+    public void unlock(int pin) {
+       if (pin == this.pin) {
+           this.locked = false;
+        } else  {
+            this.failedAttempts += 1;
+        }
+       if (getFailedAttempts() >= getMaxAttempts()) {
+           this.blocked = true;
+       }
+    }
+
+    @Override
+    public void lock() {
+        if (pin == null) {
+            throw new NullPointerException();
+        }
+        this.locked = true;
+    }
+
+    @Override
+    public boolean isLocked() {
+        return this.locked;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return this.blocked;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return MAX_ATTEMPTS_FOR_UNLOCK;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return this.failedAttempts;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,54 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SmartDoorLockTest {
+
+    public static final int PIN = 1234;
+    public static final int WRONG_PIN = 2345;
+    private MySmartDoorLock smartDoorLock;
+
+    @BeforeEach
+    void initializeSmartDoorLock() {
+        smartDoorLock = new MySmartDoorLock();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testInitializeSmartDoorLock() {
+        assertFalse(smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testSetPin() {
+        smartDoorLock.setPin(PIN);
+        smartDoorLock.lock();
+        assertTrue(smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testPinIsNotSet() {
+        assertThrows(NullPointerException.class, () -> {smartDoorLock.lock();});
+    }
+
+    @Test
+    public void testUnlockTheDoor() {
+        smartDoorLock.setPin(PIN);
+        smartDoorLock.lock();
+        smartDoorLock.unlock(PIN);
+        assertFalse(smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testFailedUnlockTheDoor() {
+        smartDoorLock.setPin(PIN);
+        smartDoorLock.lock();
+        smartDoorLock.unlock(WRONG_PIN);
+        smartDoorLock.unlock(WRONG_PIN);
+        smartDoorLock.unlock(WRONG_PIN);
+        assertTrue(smartDoorLock.isBlocked());
     }
 }
